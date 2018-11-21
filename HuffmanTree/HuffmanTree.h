f9b13f386e95cfb2a1594662ff10fc06d710ac4f
吴迪 <1443307319@qq.com>
2018-11-21 18:15:48 +0800

    add comments

--- HuffmanTree/HuffmanTree.h
+++ HuffmanTree/HuffmanTree.h
@@ -2,11 +2,13 @@
 
 #include "MinHeap.h"
 #include "TreeNode.h"
-#include <list>
+#include <chrono>
 #include <map>
-#include <stack>
+#include <numeric>
+#include <ratio>
 #include <string>
 #include <vector>
+
 
 // 哈夫曼树类模板
 template <class CharType, class WeightType>
@@ -79,19 +81,31 @@ public:
     //使用std::unique_ptr<HuffmanTreeNode>会导致root与子节点类型不同
     std::unique_ptr<TreeNode<HuffmanTreeNodeContent>> root;
     std::map<CharType, std::string> codes;
-    void MakeCodes(decltype(&root) current, std::string& code);
+    void MakeCodes(decltype(&root) current, std::string & code);
 public:
-    //  哈夫曼树方法声明及重载编译系统默认方法声明:
-    HuffmanTree(const CharType ch[], const WeightType w[], int n);  // 由字符,权值和字符个数构造哈夫曼树
+    // 哈夫曼树方法声明及重载编译系统默认方法声明:
+    // 由字符,权值和字符个数构造哈夫曼树
+    HuffmanTree(const CharType ch[], const WeightType w[], int n);
+    // 由字符,权值向量构造哈夫曼树
     HuffmanTree(const std::vector<CharType>& chars, const std::vector<WeightType>& weights) :
-        HuffmanTree(chars.data(), weights.data(), (int)chars.size()) {}
-    //virtual ~HuffmanTree();                          // 析构函数模板
-    std::string Encode(CharType ch);                 // 编码
-    std::string Encode(std::vector<CharType> chars);                 // 编码
-    std::list<CharType> Decode(const std::string& strCode);  // 译码
-    HuffmanTree(const HuffmanTree<CharType, WeightType> &copy); // 复制构造函数模板
-    HuffmanTree<CharType, WeightType> &operator=(const HuffmanTree<CharType,
-        WeightType>& copy);                     // 重载赋值运算符
+        // 调用已有构造函数
+        HuffmanTree(chars.data(), weights.data(), (int)chars.size())
+    {}
+    // 移动构造函数模板
+    HuffmanTree(HuffmanTree &&other) :
+        codes(std::move(other.codes)), 
+        root(std::move(other.root)) 
+    {}
+    // 使用默认析构函数模板
+    //virtual ~HuffmanTree();
+    // 编码字符
+    std::string Encode(CharType ch); 
+    // 编码字符串
+    std::string Encode(std::vector<CharType> chars);
+    // 译码
+    std::vector<CharType> Decode(const std::string& strCode);
+    // 重载移动赋值运算符
+    HuffmanTree &operator=(HuffmanTree&& copy);
 };
 
 
@@ -99,23 +113,16 @@ public:
 template<class CharType, class WeightType>
 inline void HuffmanTree<CharType, WeightType>::MakeCodes(decltype(&root) current, std::string & code)
 {
-    if ((*current)->IsLeaf())
+    if ((*current)->left == nullptr)
     {
         this->codes.insert({ (*current)->content.ch,code });
         return;
     }
-    if ((*current)->left != nullptr)
-    {
-        code.push_back('0');
-        this->MakeCodes(&(*current)->left, code);
-        code.pop_back();
-    }
-    if ((*current)->right != nullptr)
-    {
-        code.push_back('1');
-        this->MakeCodes(&(*current)->right, code);
-        code.pop_back();
-    }
+    code.push_back('0');
+    this->MakeCodes(&(*current)->left, code);
+    code.back() = '1';
+    this->MakeCodes(&(*current)->right, code);
+    code.pop_back();
 }
 
 template<class CharType, class WeightType>
@@ -149,21 +156,100 @@ inline HuffmanTree<CharType, WeightType>::HuffmanTree(const CharType ch[], const
     {
         this->root = std::make_unique<HuffmanTreeNode>(minHeap.RemoveMin());
     }
+
+
+    const auto time0 = std::chrono::high_resolution_clock::now();
+
+    /*std::thread t1, t2, t3, t4;
+    std::string code1("00"), code2("01"),code3("10"), code4("11");
+    if (root->left->left != nullptr)
+    {
+        t1 = std::thread(&HuffmanTree::MakeCodes, this, &root->left->left, std::ref(code1));
+        t2 = std::thread(&HuffmanTree::MakeCodes, this, &root->left->right, std::ref(code2));
+    }
+    if (root->right->left != nullptr)
+    {
+        t3 = std::thread(&HuffmanTree::MakeCodes, this, &root->right->left, std::ref(code3));
+        t4 = std::thread(&HuffmanTree::MakeCodes, this, &root->right->right, std::ref(code4));
+    }
+    t1.join();
+    t2.join();
+    t3.join();
+    t4.join();*/
+    /*std::string code1("0"), code2("1");
+    std::thread t1(&HuffmanTree::MakeCodes, this, &root->left, std::ref(code1));
+    std::thread t2(&HuffmanTree::MakeCodes, this, &root->right, std::ref(code2));
+    t1.join();
+    t2.join();*/
     std::string code;
-    this->MakeCodes(&root, code);
+    this->MakeCodes(&this->root, code);
+
+    const auto time1 = std::chrono::high_resolution_clock::now();
+    std::cout <<"MakeCodes: "<<
+        std::chrono::duration_cast<std::chrono::duration<double, std::milli>>
+        (time1 - time0).count() << "(ms)\n";
+
+}
+
+template<class CharType, class WeightType>
+inline std::string HuffmanTree<CharType, WeightType>::Encode(CharType ch)
+{
+    return this->codes.find(ch)->second;
 }
 
 template<class CharType, class WeightType>
 std::string HuffmanTree<CharType, WeightType>::Encode(std::vector<CharType> chars)
 {
-    return std::string();
+    std::string codedString;
+    for (const auto& ch : chars)
+    {
+        codedString.append(this->codes.find(ch)->second);
+    }
+    return codedString;
 }
 
 template<class CharType, class WeightType>
-inline std::list<CharType> HuffmanTree<CharType, WeightType>::Decode(const std::string & strCode)
+inline std::vector<CharType> HuffmanTree<CharType, WeightType>::Decode(const std::string & strCode)
 {
+    std::vector<CharType> decodedList;
+    auto current = &this->root;
+    //如果哈夫曼树是空的
+    if ((*current)->left == nullptr)
+    {
+        return decodedList;
+    }
     for (auto it = strCode.cbegin(); it != strCode.cend(); ++it)
     {
-
+        if ((*current)->left==nullptr)
+        {
+            decodedList.push_back((*current)->content.ch);
+            current = &this->root;
+            --it;
+            continue;
+        }
+        if (*it=='0')
+        {
+            current = &(*current)->left;
+            continue;
+        }
+        current = &(*current)->right;
     }
+    //处理最后一位
+    if ((*current)->left == nullptr)
+    {
+        decodedList.push_back((*current)->content.ch);
+    }
+    return decodedList;
 }
+
+template<class CharType, class WeightType>
+inline HuffmanTree<CharType,WeightType> & HuffmanTree<CharType, WeightType>::operator=(HuffmanTree && other)
+{
+    if (this!=&other)
+    {
+        this->codes = std::move(other.codes);
+        this->root = std::move(other.root);
+    }
+    return *this;
+}
+
